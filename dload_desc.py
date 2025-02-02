8155e56615aac7c2fb1e2d66e2f510f27b8c6f07
Dongbo Shi <shidongbo@sjtu.edu.cn>
2025-02-02 21:13:29 +0800

    add multiprocess

--- dload_desc.py
+++ dload_desc.py
@@ -4,19 +4,30 @@ import os
 from lxml import etree
 import multiprocessing
 
+path = "/Volumes/WDC5/google_patents/"
+finished = "finished_24.txt"
+pt_left = "pt_left.txt"
+opt_file = "desc_left.txt"
+
+if not os.path.exists(os.path.join(path, finished)):
+    with open(os.path.join(path, finished), 'w') as file:
+        pass
+
 def dl_pt(pt):
     url = "https://patents.google.com/patent/" + pt
     r = requests.get(url)
     r.encoding = "utf-8"
     return r.text
 
-def export_desc(pt,pt_text,path):
+
+def export_desc(pt, pt_text, path, opt_file):
     tree = etree.HTML(pt_text)
     if tree is not None:
         description_lst = tree.xpath('//section[@itemprop="description"]//text()')
         description = "".join(description_lst).replace('\n', "newline")
-        with open(path+ 'desc_23.txt', mode='a', encoding='utf-8') as fj:
+        with open(os.path.join(path, opt_file), mode='a', encoding='utf-8') as fj:
             fj.write("{1}{0}{2}\r\n".format("|", pt, description))
+
 
 # def export_cite(pt,pt_text,path):
 #     tree = etree.HTML(pt_text)
@@ -31,23 +42,25 @@ def export_desc(pt,pt_text,path):
 #                 with open(path+ 'grant_cite.txt', mode='a', encoding='utf-8') as fj:
 #                     fj.write("{1}{0}{2}{0}{3}\r\n".format("|", pt, pn, ec))
 
-def d_parse(pt,path):
+def d_parse(pt, path, opt_file, finished):
     pt_text = dl_pt(pt)
-    export_desc(pt, pt_text, path)
+    export_desc(pt, pt_text, path, opt_file)
     # export_cite(pt, pt_text, path)
-    with open(path + "finish.txt", "a") as g:
+    with open(os.path.join(path, finished), "a") as g:
         g.write(pt + "\n")
 
-if __name__ == "__main__":
-    path = "/Volumes/WDC5/google_patents/"
-    os.chdir(path)
-    with open("finish.txt", 'r') as file:
+
+if __name__ == '__main__':
+    if not os.path.exists(os.path.join(path, finished)):
+        with open(os.path.join(path, finished), 'w') as file:
+            pass
+    with open("finished_24.txt", 'r') as file:
         finish = [line.strip() for line in file.readlines()]
-    with open("cnpinfo1_2023.txt","r") as f:
-        pts = [line.replace("\n","") for line in f.readlines()]
+    with open("pt_left.txt", "r") as f:
+        pts = [line.replace("\n", "") for line in f.readlines()]
     pool = multiprocessing.Pool(12)
     for pt in pts:
         if pt not in finish:
-            pool.apply_async(func=d_parse, args=(pt, path, ))
+            pool.apply_async(func=d_parse, args=(pt, path, opt_file, finished))
     pool.close()
     pool.join()
--- patent_parser.py
+++ patent_parser.py
@@ -1,25 +1,36 @@
 import os
 import json
-ipt_dirt = "/Volumes/WDC3/google_patents/publicationzip/"
-opt_dirt = "/Volumes/WDC3/google_patents/tidydata/"
-files = os.listdir(ipt_dirt)
-#count = 0
-#for file in files:
-#    count += 1
-#    if file == 'data000000001871':
-#        print(count)
-#        break
-# os.chdir("/Users/birdstone/Dropbox/CnCitation/newdata/google")
-# with open('sample.txt',"r") as f:
-#     for pt in f.readlines():
-#         patent = json.loads(pt)
+import multiprocessing
 
-for file in files:
-    print(file)
-    if file.startswith('data'):
-        with open(ipt_dirt+file,"r") as f:
+ipt_dirt = "/Volumes/WDC3/google_patents/publications/"
+opt_dirt = "/Volumes/WDC3/google_patents/tidydata/"
+finished = "finished.txt"
+
+if not os.path.exists(os.path.join(opt_dirt, finished)):
+    with open(os.path.join(opt_dirt, finished), 'w') as file:
+        pass
+
+def parser(file, ipt_dirt, opt_dirt, finished):
+    if file.startswith('ggpatentdata'):
+        with open(ipt_dirt+file, "r") as f:
             for pt in f.readlines():
                 patent = json.loads(pt)
+                with open(opt_dirt+'app_pub_number.txt','a') as fs:
+                    fs.write("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}\n".format("|", patent["publication_number"],
+                                                      patent["application_number"],
+                                                      patent["country_code"],
+                                                      patent["application_kind"],
+                                                      patent["pct_number"],
+                                                      patent["family_id"]))
+                with open(opt_dirt+'date.txt','a') as fs:
+                    fs.write("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}\n".format("|",patent["publication_number"],
+                                                      patent["application_number"],
+                                                      patent["publication_date"],
+                                                      patent["filing_date"],
+                                                      patent["grant_date"],
+                                                      patent["priority_date"]))
+        with open(os.path.join(opt_dirt, finished), 'a') as fs:
+            fs.write(file+'\n')
                 # if len(patent["title_localized"]) > 0:
                 #     with open(opt_dirt+'title.txt','a') as fs:
                 #         fs.write("{1}{0}{2}\n".format("|",patent["application_number"],
@@ -38,36 +49,33 @@ for file in files:
                 #              'descrip':patent["description_localized"][0]["text"]}
                 #     with open(opt_dirt+'description.txt','a') as fs:
                 #         fs.write(json.dumps(descr)+'\n')
-                # # if len(patent["ipc"]) > 0:
+                # if len(patent["ipc"]) > 0:
                 #     for ipc in patent["ipc"]:
                 #         with open(opt_dirt + 'ipc.txt', 'a') as fs:
                 #             fs.write("{1}{0}{2}\n".format("|",patent["application_number"],
                 #                                           ipc["code"]))
-                # with open(opt_dirt+'app_pub_number.txt','a') as fs:
-                #     fs.write("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}\n".format("|", patent["publication_number"],
-                #                                       patent["application_number"],
-                #                                       patent["country_code"],
-                #                                       patent["application_kind"],
-                #                                       patent["pct_number"],
-                #                                       patent["family_id"]))
-                # with open(opt_dirt+'date.txt','a') as fs:
-                #     fs.write("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}\n".format("|",patent["publication_number"],
-                #                                       patent["application_number"],
-                #                                       patent["publication_date"],
-                #                                       patent["filing_date"],
-                #                                       patent["grant_date"],
-                #                                       patent["priority_date"]))
-                if len(patent["citation"]) > 0:
-                    for cite in patent["citation"]:
-                        if cite["npl_text"].__len__() > 0:
-                            with open(opt_dirt+"npc.txt","a") as fs:
-                                fs.write("{1}{0}{2}{0}{3}\n".format("|",patent["publication_number"],
-                                                                  cite["npl_text"],cite["category"]))
-                        else:
-                            with open(opt_dirt+"backward.txt","a") as fs:
-                                fs.write("{1}{0}{2}{0}{3}\n".format("|",patent["publication_number"],
-                                                                  cite["publication_number"],
-                                                                  cite["application_number"],
-                                                                  cite["type"],
-                                                                  cite["category"]))
+                # if len(patent["citation"]) > 0:
+                #     for cite in patent["citation"]:
+                #         if cite["npl_text"].__len__() > 0:
+                #             with open(opt_dirt+"npc.txt","a") as fs:
+                #                 fs.write("{1}{0}{2}{0}{3}\n".format("|",patent["publication_number"],
+                #                                                   cite["npl_text"],cite["category"]))
+                #         else:
+                #             with open(opt_dirt+"backward.txt","a") as fs:
+                #                 fs.write("{1}{0}{2}{0}{3}\n".format("|",patent["publication_number"],
+                #                                                   cite["publication_number"],
+                #                                                   cite["application_number"],
+                #                                                   cite["type"],
+                #                                                   cite["category"]))
 
+if __name__ == '__main__':
+    files = os.listdir(ipt_dirt)
+    with open(os.path.join(opt_dirt, finished), 'r') as file:
+        finish = [line.strip() for line in file.readlines()]
+    pool = multiprocessing.Pool(12)
+    for file in files:
+        print(file)
+        if file not in finish:
+            pool.apply_async(func=parser, args=(file, ipt_dirt, opt_dirt, finished,))
+    pool.close()
+    pool.join()
